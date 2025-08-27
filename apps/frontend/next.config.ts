9897d1dff570728a53e4ebc2fbad959b8489f676
Ben Horner <info@benhorner.dev>
2025-08-28 11:39:41 +1200

    feat: port 44 setup posthog (#47)
    * chore: add posthog deps
    
    * feat: add posthog rule
    
    * feat: add posthog config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,21 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+		];
+	},
+	skipTrailingSlashRedirect: true,
+};
 
 export default withSentryConfig(nextConfig, {
 	org: "benhornerdev",
--- apps/frontend/src/instrumentation-client.ts
+++ apps/frontend/src/instrumentation-client.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
+import posthog from "posthog-js";
 import { Environment } from "./lib/constants";
 
 Sentry.init({
@@ -10,6 +11,14 @@ Sentry.init({
 	replaysOnErrorSampleRate: 1.0,
 	debug: false,
 	environment: process.env.VERCEL_ENV || Environment.DEVELOPMENT,
+});
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY || "";
+posthog.init(POSTHOG_KEY, {
+	api_host: "/ingest",
+	ui_host: "https://us.posthog.com",
+	defaults: "2025-05-24",
+	capture_exceptions: true,
+	debug: process.env.NODE_ENV === "development",
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
