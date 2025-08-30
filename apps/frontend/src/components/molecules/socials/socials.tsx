5895bc79c34c243f83926e454b90d94c18367652
Ben Horner <info@benhorner.dev>
2025-08-30 22:58:26 +1200

    feat: socials

---
+++ apps/frontend/src/components/molecules/socials/socials.tsx
@@ -0,0 +1,13 @@
+import type { SocialLink } from "@/components/atoms/socialLink";
+
+interface SocialsProps {
+	links: React.ReactElement<React.ComponentProps<typeof SocialLink>>[];
+}
+
+export function Socials({ links }: SocialsProps) {
+	return (
+		<div className="mt-8 flex flex-col sm:flex-row gap-6 justify-center items-center">
+			{links}
+		</div>
+	);
+}
