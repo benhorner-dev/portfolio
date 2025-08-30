2db4282d4bd93c6701b03396aefec39c7fcc166b
Ben Horner <info@benhorner.dev>
2025-08-30 22:47:56 +1200

    feat: add content config errors

---
+++ apps/frontend/src/lib/errors.ts
@@ -0,0 +1,21 @@
+export class ContentConfigError extends Error {
+	constructor(message: string, cause?: Error) {
+		super(message);
+		this.name = "ContentConfigError";
+		this.cause = cause;
+	}
+}
+
+export class ContentConfigLoadError extends ContentConfigError {
+	constructor(message: string, cause?: Error) {
+		super(message, cause);
+		this.name = "ContentConfigLoadError";
+	}
+}
+
+export class ContentConfigParseError extends ContentConfigError {
+	constructor(message: string, cause?: Error) {
+		super(message, cause);
+		this.name = "ContentConfigParseError";
+	}
+}
