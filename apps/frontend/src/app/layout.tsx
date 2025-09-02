767593b284e73f05bd46bf72fcff6e6ac5dd434b
Ben Horner <info@benhorner.dev>
2025-09-02 22:54:36 +1200

    feat: session with login btns in layout

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { LogoButton } from "@/components/atoms/logoButton";
 import { NavigationButton } from "@/components/atoms/navigationButton";
 import { Navigation } from "@/components/organisms/navigation";
 import { getContentConfig } from "@/lib/getContentConfig";
+import { auth0 } from "@/lib/identity/auth0";
 
 const jetbrainsMono = localFont({
 	src: "./fonts/JetBrainsMonoNL-Regular.ttf",
@@ -15,12 +16,13 @@ const jetbrainsMono = localFont({
 export const metadata: Metadata = {
 	title: "Ben Horner's Portfolio",
 	description: "Ben Horner's Portfolio",
-	viewport: {
-		width: "device-width",
-		initialScale: 1,
-		maximumScale: 1,
-		userScalable: false,
-	},
+};
+
+export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
 };
 
 export default async function RootLayout({
@@ -41,12 +43,21 @@ export default async function RootLayout({
 		<NavigationButton key={navBtnId} href={`#${navBtnId}`} text={navBtnId} />
 	));
 
+	const session = await auth0.getSession();
+	const loggedIn = session?.user?.name;
+	const loginButton = (
+		<NavigationButton
+			key="login"
+			href={loggedIn ? "/auth/logout" : "/auth/login"}
+			text={loggedIn ? "Logout" : "Login"}
+		/>
+	);
 	return (
 		<html lang="en">
 			<body className={`${jetbrainsMono.variable} antialiased`}>
 				<Navigation
 					logoButton={logoButton}
-					navigationButtons={navigationButtons}
+					navigationButtons={[...navigationButtons, loginButton]}
 				/>
 				{children}
 			</body>
